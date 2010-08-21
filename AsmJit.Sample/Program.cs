8d2c250dc97e412d9471e6c8112f4f44a94156ea
sam <sam@bebc6eb5-3b83-4648-ab5f-1912f65ca8c3>
2010-08-21 01:59:47 +0000

    Add AsmJit.Sample which I can use to test 64-bit code generation
    git-svn-id: https://terabyte:8443/svn/AsmJit/trunk@24 bebc6eb5-3b83-4648-ab5f-1912f65ca8c3

---
+++ AsmJit.Sample/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AsmJit.Sample
+{
+    using System;
+    using AsmJitNet2;
+
+    internal class Program
+    {
+        [System.Runtime.InteropServices.UnmanagedFunctionPointer(System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        private delegate int Method1();
+
+        private static void Main(string[] args)
+        {
+            Compiler compiler = new Compiler();
+            CallingConvention callingConvention = (IntPtr.Size == 4) ? CallingConvention.Cdecl : CallingConvention.X64W;
+            compiler.NewFunction(callingConvention, typeof(Func<int>));
+            GPVar var = compiler.NewGP();
+            compiler.Mov(var, 3);
+            compiler.Ret(var);
+            compiler.EndFunction();
+            IntPtr ptr = compiler.Make();
+            Method1 method = (Method1)System.Runtime.InteropServices.Marshal.GetDelegateForFunctionPointer(ptr, typeof(Method1));
+            int result = method();
+            Console.WriteLine(result);
+        }
+    }
+}
