3de5083aaa77ce19fcdaeb11b146578e19c3c9f8
sam <sam@bebc6eb5-3b83-4648-ab5f-1912f65ca8c3>
2010-08-24 21:35:21 +0000

    Add a unit test that highlight the bug in a backwards jump to an "unreachable" label
    git-svn-id: https://terabyte:8443/svn/AsmJit/trunk@43 bebc6eb5-3b83-4648-ab5f-1912f65ca8c3

---
+++ AsmJitNetTest/TestJumps.cs
@@ -0,0 +1,32 @@
+﻿namespace AsmJitNetTest
+{
+    using System;
+    using AsmJitNet;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestJumps
+    {
+        [TestMethod]
+        public void TestReverseJumpToUnreachableLabel()
+        {
+            Compiler compiler = new Compiler();
+            compiler.NewFunction(CallingConvention.Default, typeof(Action));
+
+            Label a = compiler.NewLabel();
+            Label b = compiler.NewLabel();
+            Label c = compiler.NewLabel();
+
+            compiler.Jmp(b);
+            compiler.Bind(a);
+            compiler.Jmp(c);
+            compiler.Bind(b);
+            compiler.Jmp(a);
+            compiler.Bind(c);
+            compiler.Ret();
+            compiler.EndFunction();
+
+            IntPtr result = compiler.Make();
+        }
+    }
+}
